eea354f9cc45dfb72ce6e64568a6a8abb1f2c924
dombics <154469541+dombics@users.noreply.github.com>
2025-03-18 18:54:34 +0100

    Add GetAllArticlesQuery

---
+++ BlazorRealTimeApp.Application/Articles/GetArticles/GetAllArticlesQuery.cs
@@ -0,0 +1,12 @@
+﻿using BlazorRealTimeApp.Domain.Articles;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRealTimeApp.Application.Articles.GetArticles
+{
+    public record GetAllArticlesQuery : IRequest<List<Article>>;
+}
---
+++ BlazorRealTimeApp.Application/Articles/GetArticles/GetAllArticlesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using BlazorRealTimeApp.Domain.Articles;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRealTimeApp.Application.Articles.GetArticles
+{
+    public class GetAllArticlesQueryHandler : IRequestHandler<GetAllArticlesQuery, List<Article>>
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public GetAllArticlesQueryHandler(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<List<Article>> Handle(GetAllArticlesQuery request, CancellationToken cancellationToken)
+        {
+            var article = await _articleRepository.GetAllArticlesAsync();
+            return article;
+        }
+    }
+}
