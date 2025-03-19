49ec354a7de8673d5d577b501fd8961a1f14347e
dombics <154469541+dombics@users.noreply.github.com>
2025-03-19 10:14:30 +0100

    Implemented SeriLog App Logger.

--- BlazorRealTimeApp.Infrastructure/DependencyInjection.cs
+++ BlazorRealTimeApp.Infrastructure/DependencyInjection.cs
@@ -3,6 +3,7 @@ using BlazorRealTimeApp.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,15 @@ namespace BlazorRealTimeApp.Infrastructure
             this IServiceCollection services,
             IConfiguration configuration)
         {
+            // Configure Serilog from appsettings.json
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            // Add Serilog to the service collection
+            services.AddLogging(loggingBuilder =>
+                loggingBuilder.AddSerilog(dispose: true));
+
             services.AddDbContext<ApplicationDbContext>(options =>
                  options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
